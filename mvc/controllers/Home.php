b5055c6613f7f756fd7cce187b41ed8cf7319d9e
phanquangdev <phanquangdev>
2022-06-17 15:47:48 +0700

    create mvc

---
+++ mvc/controllers/Home.php
@@ -0,0 +1,17 @@
+<!-- controllers-home -->
+
+<?php 
+  Class Home {
+
+    
+    function SayHi() {
+      echo "SayHi";
+    }
+
+    function GoodBye () {
+      echo "GoodBye";
+
+    }
+  }
+
+?>
