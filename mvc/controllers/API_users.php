56de20fe103e3d2ef9307d56b6baa991d12a24cd
phanquangdev <phanquangdev>
2022-06-23 11:28:04 +0700

    created api user

--- mvc/controllers/API_users.php
+++ mvc/controllers/API_users.php
@@ -1,13 +1,39 @@
 <?php 
+  class InfoUsers {
+    public $id;
+    public $name;
+    public $email;
+    public $password;
+
+    function __construct($id,$name,$email,$password)
+    {
+      $this->id =$id;
+      $this->name =$name;
+      $this->email =$email;
+      $this->password =$password;
+
+    }
+  }
   class API_users extends Controllers{
     public function getUser(){
       $getUsers = $this->modal('Users');
+      $users =  $getUsers->getSV();
       $data =[];
 
-      array_push($data,$getUsers->getSV());
-      echo json_encode($data);
+      while($r = mysqli_fetch_array($users)){
+
+        array_push($data, new InfoUsers(
+          $r['id'],
+          $r['name'],
+          $r['email'],
+          $r['password'],
+        ));
+
+      }
+      echo json_encode ($data);
+
     }
   }
-?>
 
-<!-- http://localhost:8081/learn-php-basic/API_users/getUser -->
+  
+?>
